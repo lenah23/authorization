78e913beaabc3df5fd663c53407217c66afcf47e
lenah23 <lenulstep@gmail.com>
2025-02-16 17:16:31 +0400

    ready

--- src/app/components/Industries/AutoComplete.tsx
+++ src/app/components/Industries/AutoComplete.tsx
@@ -1,10 +1,10 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Input from '../Input/Input';
 import searchIcon from '../../assets/images/search-icon.svg';
+import UseIndustriesHooks from './Industries.hooks';
 import styles from './Industries.module.scss';
-import { toast } from 'react-toastify';
 
 interface AutocompleteInputProps {
   options: { id: number; name: string }[];
@@ -12,50 +12,15 @@ interface AutocompleteInputProps {
 }
 
 const AutocompleteInput: React.FC<AutocompleteInputProps> = ({ options }) => {
-  const [isFocused, setIsFocused] = useState(false);
-  const [searchValue, setSearchValue] = useState<string>('');
-  const [filteredOptions, setFilteredOptions] = useState(options);
-  const [selectedIndustries, setSelectedIndustries] = useState<
-    { id: number; name: string }[]
-  >([]);
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const value = event.target.value;
-    setSearchValue(value);
-  };
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      const filtered = options?.filter((option) =>
-        option.name.toLowerCase().includes(searchValue.toLowerCase())
-      );
-      setFilteredOptions(filtered);
-    }, 300);
-
-    return () => clearTimeout(timer);
-  }, [searchValue, options]);
-
-  const handleItemClick = (industry: { id: number; name: string }) => {
-    const isIndustrySelected = selectedIndustries.some(
-      (selected) => selected.id === industry.id
-    );
-
-    if (!isIndustrySelected) {
-      if (selectedIndustries.length < 3) {
-        setSelectedIndustries((prevSelected) => [...prevSelected, industry]);
-      } else {
-        toast.warning('You can only select up to 3 industries.');
-      }
-    }
-
-    setIsFocused(false);
-  };
-
-  const handleRemoveIndustry = (id: number) => {
-    setSelectedIndustries((prevSelected) =>
-      prevSelected.filter((industry) => industry.id !== id)
-    );
-  };
+  const {
+    isFocused,
+    filteredOptions,
+    selectedIndustries,
+    handleRemoveIndustry,
+    handleItemClick,
+    setIsFocused,
+    handleChange,
+  } = UseIndustriesHooks({ options });
 
   return (
     <div className={styles['autocomplete-container']}>
@@ -68,15 +33,17 @@ const AutocompleteInput: React.FC<AutocompleteInputProps> = ({ options }) => {
       {filteredOptions && isFocused && (
         <div className={styles['options-container']}>
           {filteredOptions?.length > 0 ? (
-            filteredOptions?.map((option, index) => (
-              <div
-                key={index}
-                className={styles['option-item']}
-                onClick={() => handleItemClick(option)}
-              >
-                {option?.name}
-              </div>
-            ))
+            filteredOptions?.map(
+              (option: { id: number; name: string }, index: number) => (
+                <div
+                  key={index}
+                  className={styles['option-item']}
+                  onClick={() => handleItemClick(option)}
+                >
+                  {option?.name}
+                </div>
+              )
+            )
           ) : (
             <div className={styles['empty']}>Nothing has been found</div>
           )}
--- src/app/components/Industries/Industries.hooks.ts
+++ src/app/components/Industries/Industries.hooks.ts
@@ -1,10 +1,71 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
 
-const UseIndustriesHooks = () => {
+interface IProps {
+  options: { id: number; name: string }[];
+}
+
+const UseIndustriesHooks = ({ options }: IProps) => {
   const [selectedValue, setSelectedValue] = useState<string>('');
-  return { selectedValue, setSelectedValue };
+  const [isFocused, setIsFocused] = useState(false);
+  const [searchValue, setSearchValue] = useState<string>('');
+  const [filteredOptions, setFilteredOptions] = useState(options);
+  const [selectedIndustries, setSelectedIndustries] = useState<
+    { id: number; name: string }[]
+  >([]);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setSearchValue(value);
+  };
+
+  const handleItemClick = (industry: { id: number; name: string }) => {
+    const isIndustrySelected = selectedIndustries.some(
+      (selected) => selected.id === industry.id
+    );
+
+    if (!isIndustrySelected) {
+      if (selectedIndustries.length < 3) {
+        setSelectedIndustries((prevSelected) => [...prevSelected, industry]);
+      } else {
+        toast.warning('You can only select up to 3 industries.');
+      }
+    }
+    setIsFocused(false);
+  };
+
+  const handleRemoveIndustry = (id: number) => {
+    setSelectedIndustries((prevSelected) =>
+      prevSelected.filter((industry) => industry.id !== id)
+    );
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      const filtered = options?.filter((option) =>
+        option.name.toLowerCase().includes(searchValue.toLowerCase())
+      );
+      setFilteredOptions(filtered);
+    }, 300);
+
+    return () => clearTimeout(timer);
+  }, [searchValue, options]);
+
+  return {
+    isFocused,
+    searchValue,
+    selectedValue,
+    filteredOptions,
+    selectedIndustries,
+    handleRemoveIndustry,
+    setFilteredOptions,
+    setSelectedValue,
+    handleItemClick,
+    setIsFocused,
+    handleChange,
+  };
 };
 
 export default UseIndustriesHooks;
